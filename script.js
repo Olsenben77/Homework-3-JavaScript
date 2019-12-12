9ddcdf60db69d0b0b2e17d174a75374d9a666008
Ben Olsen <olsenben1992@gmail.com>
2019-12-11 22:24:20 -0800

    Homework Changes 2

--- script.js
+++ script.js
@@ -1,50 +1,55 @@
 
+
+var lower = "abcdefghijklmnopqrstuvwxyz".split('');
+var numbers = "1234567890".split('');
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split('');
+
 var howManyCharacters = prompt('How many characters would you like your password to contain?');
 console.log(howManyCharacters > 128)
 
-for (var i=howManyCharacters; howManyCharacters > 128 || howManyCharacters < 8; howManyCharacters = prompt('How many characters would you like your password to contain?')) {
-if(howManyCharacters > 128){
-    alert('Password is too long!');
+for (var i = howManyCharacters; howManyCharacters > 128 || howManyCharacters < 8; howManyCharacters = prompt('How many characters would you like your password to contain?')) {
+    if (howManyCharacters > 128) {
+        alert('Password is too long!');
     }
+
+    if (howManyCharacters < 8) {
+        alert('Password is too short!');
+    }
+}
+
+var wantsLowercase = confirm("Do you want to include lowercase alphabet?"); 
+
+var wantsNumbers = confirm("Do you want to include numbers?"); 
+
+var wantsUpper = confirm("Do you want to include uppercase alphabet?"); 
+
+var wantsSpecialCharacters = confirm("Do you want to include Special Characters?"); 
+
+if(!wantsLowercase && !wantsNumbers && !wantsUpper && !wantsSpecialCharacters){
     
-    if(howManyCharacters < 8){
-    alert('Password is too short!');
-    }
-}
+    wantsLowercase = confirm("Do you want to include lowercase alphabet?");
+     
+    wantsNumbers = confirm("Do you want to include numbers?"); 
+    
+    wantsUpper = confirm("Do you want to include uppercase alphabet?"); 
+    
+    wantsSpecialCharacters = confirm("Do you want to include Special Characters?"); 
+
+} 
+console.log(wantsLowercase, wantsNumbers, wantsUpper, wantsSpecialCharacters,);
 
 
-function getRandomLower () {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-    const symbols = '!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
-    return symbols[Math.floor(Math.random() * symbols.length)];
-}
+let password = "";
 
 
-var alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-var numbers = "1234567890".toCharArray();
-var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-var specialCharacters = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" .toCharArray();
-
-let password ="";
-
-for (var i=0; i <= complexity; i++){
-    password = password = values.charAt(Math.floor(Math.random() * Math.floor(value.length-1)));
-}
+// for (var i = 0; i <= complexity; i++) {
+//     password = password = values.charAt(Math.floor(Math.random() * Math.floor(value.length - 1)));
+// }
 
 
 
-function copyPassword(){
+function copyPassword() {
 
     document.getElementById("display").select();
 
@@ -52,4 +57,4 @@ function copyPassword(){
     alert("Password copied to Clipboard!");
 }
 
-GenerateButton.addEventListener('click',toggleMode);
+GenerateButton.addEventListener('click', toggleMode);
